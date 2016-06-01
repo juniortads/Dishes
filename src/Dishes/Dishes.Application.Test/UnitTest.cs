61a7f540b69aaaf57d03560e41bac26a9dff3bd5
Joel Júnior <junior.tads@gmail.com>
2016-06-01 00:45:21 -0300

    Create unit test

---
+++ src/Dishes/Dishes.Application.Test/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dishes.Infra.IoC;
+
+namespace Dishes.Application.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private IServiceAppMenu _serviceAppMenu;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _serviceAppMenu = ContainerConfigurationDictionary.Resolve<IServiceAppMenu>();
+            //_serviceAppMenu = ContainerConfigurationUnity.Resolve<IServiceAppMenu>();
+        }
+
+        [TestMethod]
+        public void Menu_By_Morning_Dish_Type_1_2_3()
+        {
+            int[] dishTypes = new[] { 1, 2, 3 };
+            var menus = _serviceAppMenu.SearchMenuTimeOfDayByMorning(dishTypes);
+            Assert.AreEqual(dishTypes.Length, menus.Count);
+        }
+
+        [TestMethod]
+        public void Menu_By_Morning_Dish_Type_2_1_3()
+        {
+            int[] dishTypes = new[] { 2, 1, 3 };
+            var menus = _serviceAppMenu.SearchMenuTimeOfDayByMorning(dishTypes);
+            Assert.AreEqual(dishTypes.Length, menus.Count);
+        }
+    }
+}
